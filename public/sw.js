ca010efdc7b3dc01d9225b742be1203f4cb5864f
midoon <harunkoding@gmail.com>
2025-05-19 20:26:19 +0700

    Init-offline-first

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,5 @@
 const CACHE_NAME = "offline-v1";
 
-const preLoad = function () {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        // caching index and important routes
-        return cache.addAll(filesToCache);
-    });
-};
-
-self.addEventListener("install", function (event) {
-    event.waitUntil(preLoad());
-});
-
 const filesToCache = [
     "/",
     "/teacher/register",
@@ -22,43 +11,21 @@ const filesToCache = [
     "/images/offline.svg",
 ];
 
-const checkResponse = function (request) {
-    return new Promise(function (fulfill, reject) {
-        fetch(request).then(function (response) {
-            if (response.status !== 404) {
-                fulfill(response);
-            } else {
-                reject();
-            }
-        }, reject);
+const preLoad = () => {
+    return caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(filesToCache);
     });
 };
 
-const addToCache = function (request) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        return fetch(request).then(function (response) {
-            return cache.put(request, response);
-        });
-    });
-};
+self.addEventListener("install", (event) => {
+    event.waitUntil(preLoad());
+});
 
-const returnFromCache = function (request) {
-    return caches.open(CACHE_NAME).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            if (!matching || matching.status === 404) {
-                return cache.match("offline.html");
-            } else {
-                return matching;
-            }
-        });
-    });
-};
-
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then(function (keyList) {
+        caches.keys().then((keyList) => {
             return Promise.all(
-                keyList.map(function (key) {
+                keyList.map((key) => {
                     if (key !== CACHE_NAME) {
                         return caches.delete(key);
                     }
@@ -68,16 +35,34 @@ self.addEventListener("activate", function (event) {
     );
 });
 
-self.addEventListener("fetch", function (event) {
-    if (event.request.method !== "GET") return;
+// Offline First Strategy
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
     event.respondWith(
-        checkResponse(event.request).catch(function () {
-            return returnFromCache(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request)
+                .then((networkResponse) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    });
+                })
+                .catch(() => {
+                    if (
+                        event.request.headers
+                            .get("accept")
+                            .includes("text/html")
+                    ) {
+                        return caches.match("/offline.html");
+                    }
+                });
         })
     );
-
-    // cors filter
-    if (event.request.mode !== "cors" && event.request.url.startsWith("http")) {
-        event.waitUntil(addToCache(event.request));
-    }
 });
